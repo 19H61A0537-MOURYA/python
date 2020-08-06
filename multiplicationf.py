09adb0ec59739490bede6c7179ceaba028d6215a
19H61A0537-MOURYA <69301612+19H61A0537-MOURYA@users.noreply.github.com>
2020-08-06 15:22:22 +0530

    Add files via upload

---
+++ multiplicationf.py
@@ -0,0 +1,12 @@
+# Python Program to print table  
+ 
+  
+def table(n): 
+    for i in range (1, 11):  
+          
+        # multiples from 1 to 10 
+        print ("%d * %d = %d" % (n, i, n * i)) 
+  
+# number for which table is evaluated 
+n =int(input( "number:"))
+table(n) 
